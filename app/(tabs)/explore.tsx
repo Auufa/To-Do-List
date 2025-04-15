56175e8fa09b046f926e70bb776e150ca0a1f6f1
Auufa <aufa.irfan64@smk.belajar.id>
2025-04-15 13:28:08 +0700

    update

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,109 +1,183 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from "twrnc";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Picker } from '@react-native-picker/picker';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+const Index = () => {
+  const [mapel, setMapel] = useState('');
+  const [title, setTitle] = useState('');
+  const [deadline, setDeadline] = useState('');
+  const [category, setCategory] = useState('PR');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
 
-export default function TabTwoScreen() {
+  useEffect(() => {
+    loadTask();
+  }, []);
+
+  useEffect(() => {
+    saveTask();
+  }, [list]);
+
+  const addTask = () => {
+    if (title.trim() === '' || deadline.trim() === '') return;
+
+    const newTask = {
+      id: Date.now().toString(),
+      mapel: mapel.trim(),
+      title: title.trim(),
+      deadline: deadline.trim(),
+      category,
+      checked: false,
+    };
+
+    setList([...list, newTask]);
+    setMapel('');
+    setTitle('');
+    setDeadline('');
+    setCategory('PR');
+  };
+
+  const toggleCheck = (id: string) => {
+    const updated = list.map(item =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setList(updated);
+  };
+
+  const saveTask = async () => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(list));
+    } catch (error) {
+      console.log('Gagal simpan:', error);
+    }
+  };
+
+  const loadTask = async () => {
+    try {
+      const saved = await AsyncStorage.getItem('tasks');
+      if (saved !== null) {
+        setList(JSON.parse(saved));
+      }
+    } catch (error) {
+      console.log('Gagal load:', error);
+    }
+  };
+
+  const deleteTask = (id: string) => {
+    const filtered = list.filter(item => item.id !== id);
+    setList(filtered);
+  };
+
+  const handleEdit = () => {
+    const updated = list.map(item =>
+      item.id === editId
+        ? { ...item, mapel:mapel.trim(), title: title.trim(), deadline: deadline.trim(), category }
+        : item
+    );
+    setList(updated);
+    setMapel('');
+    setTitle('');
+    setDeadline('');
+    setCategory('PR');
+    setIsEditing(false);
+    setEditId(null);
+  };
+
+  const startEdit = (item: any) => {
+    setMapel(item.mapel);
+    setTitle(item.title);
+    setDeadline(item.deadline);
+    setCategory(item.category);
+    setIsEditing(true);
+    setEditId(item.id);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
-}
+    <SafeAreaView style={tw`flex-1 mt-8`}>
+      <View>
+        <View style={tw` px-4`}>
+          <Text style={tw`font-bold text-4xl text-center`}>📝 TugasKu</Text>
+        </View>
 
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
+        <View style={tw`mt-5 px-4 gap-3`}>
+        <View>
+          <Text>Mata Pelajaran</Text>
+          <TextInput
+            placeholder='Mata Pelajaran'
+            style={tw`border border-gray-700 rounded px-2 h-10 mt-2`}
+            value={mapel}
+            onChangeText={setMapel}
+          />
+        </View>
+        
+        <View>
+          <Text>Judul Tugas</Text>
+          <TextInput
+            placeholder='Judul Tugas'
+            style={tw`border border-gray-700 rounded px-2 h-10 mt-2`}
+            value={title}
+            onChangeText={setTitle}
+          />
+        </View>
+          
+        <View>
+          <Text>Tanggal</Text>
+           <TextInput
+            placeholder='Deadline (cth: 20 Apr 2025)'
+            style={tw`border border-gray-700 rounded px-2 h-10 mt-2`}
+            value={deadline}
+            onChangeText={setDeadline}
+          />
+        </View>
+         
+          <Picker selectedValue={category} onValueChange={(itemValue) => setCategory(itemValue)}>
+            <Picker.Item label="PR" value="PR" />
+            <Picker.Item label="Mapel" value="Mapel" />
+            <Picker.Item label="Tugas" value="Tugas" />
+          </Picker>
+
+          <TouchableOpacity onPress={isEditing ? handleEdit : addTask} style={tw`bg-blue-600 h-10 rounded-lg justify-center`}>
+            <Text style={tw`text-white text-center text-base font-bold`}>
+              {isEditing ? ' SELESAI EDIT' : 'TAMBAH TUGAS'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        
+      </View>
+
+      <View style={tw`pt-5 px-4`}>
+        <FlatList
+          data={list}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={tw`border border-gray-300 rounded p-3 mb-3`}>
+              <View style={tw`flex-row justify-between items-center`}>
+                <Text style={tw`text-gray-600 ml-2 font-semibold text-2xl`}>
+                  {item.mapel}
+                </Text>
+              </View>
+              <Text style={tw`text-gray-600 mt-1`}>Judul Tugas: {item.title}</Text>
+              <Text style={tw`text-gray-600 mt-1`}>🗓 Deadline: {item.deadline}</Text>
+              <Text style={tw`text-gray-600`}>📌 Kategori: {item.category}</Text>
+              <View style={tw`flex-row gap-3 mt-2 left-56`}>
+                <TouchableOpacity onPress={() => startEdit(item)}>
+                  <Text style={tw`text-blue-800 font-bold bg-blue-200 px-3 py-1 rounded-md `}>Edit</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => deleteTask(item.id)} style={tw`bg-red-700 px-3 py-1 rounded-md`}>
+                  <Text style={tw`text-white`}>Delete</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,134 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from "twrnc";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const index = () => {
+  const [task, setTask] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing,setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
 
-export default function HomeScreen() {
+  useEffect(() => {
+    loadTask();
+  }, []);
+
+  useEffect(() => {
+    saveTask();
+  }, [list]);
+
+  const addTask = () => {
+    if (task.trim() === '') return;
+
+    const newTask = {
+      id: Date.now().toString(),
+      title: task.trim(),
+      checked: false, // Tambahan: status ceklis
+    };
+
+    setList([...list, newTask]);
+    setTask('');
+  };
+
+  const toggleCheck = (id: string) => {
+    const updated = list.map(item =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setList(updated);
+  };
+
+  const saveTask = async () => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(list));
+      console.log('Berhasil simpan data');
+    } catch (error) {
+      console.log('Gagal simpan:', error);
+    }
+  };
+
+  const loadTask = async () => {
+    try {
+      const saved = await AsyncStorage.getItem('tasks');
+      if (saved !== null) {
+        setList(JSON.parse(saved));
+        console.log('Berhasil load data');
+      }
+    } catch (error) {
+      console.log('Gagal load:', error);
+    }
+  };
+
+  const deleteTask = (id: string) => {
+    const filtered = list.filter(item => item.id !== id);
+    setList(filtered);
+  };
+
+  const handleEdit = () => {
+    const updated = list.map(item => item.id === editId ? { ...item, title: task.trim()}: item);
+    setList(updated);
+    setTask('');
+    setIsEditing(false);
+    setEditId(null);
+  };
+
+  const startEdit = (item: any) => {
+    setTask(item.title);
+    setIsEditing(true);
+    setEditId(item.id);
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <SafeAreaView style={tw`flex-1`}>
+      <View>
+        <View style={tw`bg-yellow-500 rounded-b-full h-42`} />
+        <View style={tw`-mt-5 flex-row gap-2 px-4`}>
+          <TextInput 
+            placeholder='Tambahkan tugas..' 
+            style={tw`border-gray-900 border rounded-lg w-70 h-10 px-2`}
+            value={task} 
+            onChangeText={setTask} 
+          />
+          <TouchableOpacity onPress={isEditing ? handleEdit : addTask} style={tw`bg-yellow-500 w-25 h-10 rounded-lg justify-center`}>
+            <Text style={tw`text-white text-center text-base font-bold`}>TAMBAH</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={tw`pt-4 px-4`}>
+          <Text style={tw`font-bold text-4xl`}>👷‍♂️ Personal</Text>
+        </View>
+      </View>
+
+      <View style={tw`pt-7 px-4`}>
+        <FlatList 
+          data={list} 
+          keyExtractor={item => item.id} 
+          renderItem={({ item }) => (
+            <View style={tw`flex-row items-center gap-4 mb-3`}>
+              <TouchableOpacity onPress={() => toggleCheck(item.id)} style={tw`w-7 h-7 border-2 border-gray-700 rounded-md items-center justify-center`}>
+                {item.checked && (
+                  <Text style={tw`text-lg`}>✓</Text>
+                )}
+              </TouchableOpacity>
+
+              <Text style={tw`flex-1`}>
+                {item.title}
+              </Text>
+
+              <TouchableOpacity onPress={() => startEdit(item)}>
+                <Text style={tw`text-blue-800 font-bold`}>Edit</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => deleteTask(item.id)} style={tw`bg-red-700 w-20 h-8 rounded-md items-center justify-center`}>
+                <Text style={tw`text-white`}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+      </View>
+
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default index;
